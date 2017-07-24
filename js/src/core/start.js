f2a6636d13430a190968cfe3577b7736ad64afde
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2017-07-24 19:59:03 -0400

    Changed Colors.

--- js/src/core/start.js
+++ js/src/core/start.js
@@ -4,23 +4,19 @@
 Game.fn.start = function( size ){
 
 	// Reset Score.
+	this.currentScore = 0;
 	this.$gameScore.text("0");
+
+	// Reset Moves.
+	this.totalMoves = 0;
+	this.$gameMoves.text("0");	
 
 	// Remove Overlay
 	this.$overlay.hide();
 
 	// Resize the board if needed.
-	if ( this.board && size == this.board.size ) {
-
-		this.board.reset();
-		this.$gameBestScore.text(this.bestScore+"");
-
-	} else {
-		
-		this.initBoard( size );
-		this.$gameMoves.text("0");
-
-	}
+	if ( this.board && size == this.board.size ) 	this.board.reset();
+	else											this.initBoard( size );
 
 	return this;
 }
